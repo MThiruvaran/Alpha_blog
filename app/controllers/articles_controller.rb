9f1bc7c412c229c5d69bf26d276f1166cc419ab1
MThiruvaran <thiruvarancse@gmail.com>
2020-02-10 03:59:55 +0530

    last of CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
     def index
-        @articles = Article.all
+        @articles = Article.all 
     end
     def new 
         @article = Article.new
@@ -16,6 +16,13 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was destroyed"
+        redirect_to articles_path
     end
 
     def show
@@ -36,6 +43,9 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
     end
+
+
+  
     private
 
     def article_params
